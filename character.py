99d8df1e3cea2919db07280c59a1678673b8bc26
Liam Bowie <liambowie16@gmail.com>
2023-07-17 16:25:00 +0100

    create method that rolls randomly for ability scores and returns a dictionary of scores that can be used to create a character

--- character.py
+++ character.py
@@ -1,27 +1,42 @@
 import math
+import random
 
 class Character:
-    def __init__(self, name, abilityScores, skills):
-        self.name = name
+    def __init__(self, abilityScores):
+        self.name = ""
+        self.age = 0
         self.abilityScores = abilityScores
-        self.skills = skills
+        self.skills = []
+        self.armourClass = 10 + abilityScores["dex"]
+        self.proficiencyBonus = 2
 
-    def getMod(self, ability): 
+    def addSkill(self, skill):
+        self.skills.append(skill)
+
+    def getModifier(self, ability): 
         return math.floor((self.abilityScores[ability] - 10) / 2)
 
-scores = {"str": 15, "dex": 14, "con": 13, "wis": 12, "int": 10, "cha": 8}
-skills = []
 
-pc = Character("Percy", scores)
+def roll_random_ability_scores():
+    ability_scores = {"str": 0, "dex": 0, "con": 0, "wis": 0, "int": 0, "cha": 0}
 
-print(pc.getMod("str"))
-print(pc.getMod("dex"))
-print(pc.getMod("con"))
-print(pc.getMod("wis"))
-print(pc.getMod("int"))
-print(pc.getMod("cha"))
+    # For each player characters abilities  
+    for ability in ability_scores:
+        rolls = []
+        # roll a six sided dice four times and save each value.
+        for i in range(4):
+            rolls.append(random.randint(1, 6))
+        
+        # sort the rolls into ascending order and drop the lowest value. 
+        rolls.sort()
+        rolls.pop(0)
 
+        # Add the remaining rolls to get the final score and add that to the scores dictionary 
+        finalScore = sum(rolls)
+        ability_scores[ability] = finalScore
 
+    return ability_scores
 
-
+myScores = roll_random_ability_scores()
+print(myScores)
 
