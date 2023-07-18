2a714e175e3b2efa7b777fdc4433cac8fb397a02
Liam Bowie <liambowie16@gmail.com>
2023-07-18 17:38:15 +0100

    complete script that allows users to enter their ability scores manually or have them randomly generated following the same steps as the games intention

--- character.py
+++ character.py
@@ -2,12 +2,12 @@ import math
 import random
 
 class Character:
-    def __init__(self, abilityScores):
-        self.name = ""
+    def __init__(self):
+        self.name = ''
         self.age = 0
-        self.abilityScores = abilityScores
+        self.abilityScores = null
         self.skills = []
-        self.armourClass = 10 + abilityScores["dex"]
+        self.armourClass = 10
         self.proficiencyBonus = 2
 
     def addSkill(self, skill):
@@ -18,7 +18,7 @@ class Character:
 
 
 def roll_random_ability_scores():
-    ability_scores = {"str": 0, "dex": 0, "con": 0, "wis": 0, "int": 0, "cha": 0}
+    ability_scores = {'str': 0, 'dex': 0, 'con': 0, 'wis': 0, 'int': 0, 'cha': 0}
 
     # For each player characters abilities  
     for ability in ability_scores:
@@ -37,6 +37,46 @@ def roll_random_ability_scores():
 
     return ability_scores
 
-myScores = roll_random_ability_scores()
-print(myScores)
+# start of script
 
+## ABILITY SCORES ## 
+decision = input('Do you want to manual enter your stats or have them rolled randomly? \n\nChoose: manual or random \n\n')
+
+if decision == 'random':
+
+    print('\nBehold, as the ethereal dice dance upon the digital plane, conjuring a tapestry of randomly generated scores that shall define your character\'s inherent abilities.')
+
+    randomScores = roll_random_ability_scores()
+    
+    for ability in randomScores:
+        print(f'{ability}: {randomScores[ability]}')
+
+elif decision == 'manual':
+
+    print('\nRoll 4d6 and add the three highest together to obtain your ability scores.\n')
+
+    manual_scores = {'str': 0, 'dex': 0, 'con': 0, 'wis': 0, 'int': 0, 'cha': 0}
+    score_text = {
+        'str': 'Enter thy strength, mighty warrior, and reveal the power within (strength): ', 
+        'dex': 'Unveil the nimbleness that courses through thy veins, agile soul (dexterity): ', 
+        'con': 'Embrace the endurance that fortifies thy spirit, resilient hero (constitution): ', 
+        'wis': 'Tap into the wellspring of wisdom, wise sage, and share its depth (wisdom): ', 
+        'int': 'Let the flame of intelligence burn bright, cunning mind, as you disclose its brilliance (intelligence): ', 
+        'cha': 'Awaken the aura of charisma that captivates hearts, enchanting soul (charisma): '
+    }
+
+    for ability in manual_scores:
+
+        while manual_scores[ability] == 0:
+            try: 
+                current_score = int(input(score_text[ability]))
+            except ValueError: 
+                print('\nYou must enter a number\n')
+
+            if current_score in range(3, 19):
+                manual_scores[ability] = current_score
+            else: 
+                print('\nYour score must be between 3 and 18\n')
+
+
+    print(manual_scores)
