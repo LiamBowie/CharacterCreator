d4d07ab23a19731229e223b94fa2fe797434ce1b
Liam Bowie <liambowie16@gmail.com>
2023-07-19 14:31:14 +0100

    Add draconic ancestries and control flow for a user to choose their ancestry and have it added to their race'

--- character.py
+++ character.py
@@ -1,21 +1,49 @@
 import math
 import random
 
+class Race: 
+    def __init__(self, race_name, ability_score_increase, size, speed, languages, resistances):
+        self.race_name = race_name
+        self.ability_score_increase = ability_score_increase
+        self.size = size
+        self.speed = speed
+        self.languages = languages
+        self.resistances = resistances
+
+    def add_resitances(self, new_resistances):
+        for resistance in new_resistances:
+            self.resistances.append(resistance)
+
+class Draconic_Ancestry:
+    def __init__(self, dragon_type, damage_type, breath_weapon, saving_throw):
+        self.dragon_type = dragon_type
+        self.damage_type = damage_type
+        self.breath_weapon = breath_weapon     
+        self.saving_throw = saving_throw  
+
 class Character:
     def __init__(self):
-        self.name = ''
-        self.age = 0
-        self.abilityScores = null
+        self.race = 'none'
+        self.ability_scores = {'str': 0, 'dex': 0, 'con': 0, 'wis': 0, 'int': 0, 'cha': 0}
         self.skills = []
         self.armourClass = 10
         self.proficiencyBonus = 2
 
-    def addSkill(self, skill):
+    def add_race(self, race):
+        self.race = race
+
+        for ability in race.ability_score_increase:
+            self.ability_scores[ability] += race.ability_score_increase[ability]
+
+    def update_ability_scores(self, new_scores):
+        for ability in new_scores:
+            self.ability_scores[ability] += new_scores[ability]
+
+    def add_skill(self, skill):
         self.skills.append(skill)
 
-    def getModifier(self, ability): 
-        return math.floor((self.abilityScores[ability] - 10) / 2)
-
+    def get_modifier(self, ability): 
+        return math.floor((self.ability_scores[ability] - 10) / 2)
 
 def roll_random_ability_scores():
     ability_scores = {'str': 0, 'dex': 0, 'con': 0, 'wis': 0, 'int': 0, 'cha': 0}
@@ -37,19 +65,67 @@ def roll_random_ability_scores():
 
     return ability_scores
 
-# start of script
+## Create a list of available classes (race_name, ability_score_increase, size, speed, languages) ## 
+
+
+## Available races
+dragonborn = Race('Dragonborn', {'str': 2, 'cha': 1}, 'medium', 30, ['common', 'draconic'], [])
+mountain_dwarf = Race('Mountain Dwarf', {'str': 2, 'con': 2}, 'medium', 25, ['common', 'dwarvish'], ['poison'])
+
+## Draconic Ancestries
+ancestries = {
+    'black'  : Draconic_Ancestry('black', 'acid', '5 by 30ft line', 'dex'),
+    'blue'   : Draconic_Ancestry('blue', 'lightning', '5 by 30ft line', 'dex'),
+    'brass'  : Draconic_Ancestry('brass', 'fire', '5 by 30ft line', 'dex'),
+    'bronze' : Draconic_Ancestry('bronze', 'lightning', '5 by 30ft line', 'dex'),
+    'copper' : Draconic_Ancestry('copper', 'acid', '5 by 30ft line', 'dex'),
+    'gold'   : Draconic_Ancestry('gold', 'fire', '15ft cone', 'dex'),
+    'green'  : Draconic_Ancestry('green', 'poison', '15ft cone', 'con'),
+    'red'    : Draconic_Ancestry('red', 'fire', '15ft cone', 'dex'),
+    'silver' : Draconic_Ancestry('silver', 'cold', '15ft cone', 'con'),
+    'white'  : Draconic_Ancestry('white', 'cold', '15ft cone', 'con')
+}
+
+player = Character()
+
+## RACE ## 
+#chosen_race = input('Amidst the vast expanse of fantasy, which race shall thee assume on this odyssey? Choose wisely, for destiny eagerly awaits your decision. Enter the name of your chosen race: ')
+chosen_race = 'dragonborn'
+
+if chosen_race == 'dragonborn':
+
+    dragonborn.ancestry = 'none'
+    player.add_race(dragonborn)
+
+    print('Dragonborn of noble kin, reveal your Draconic Ancestry - Red, Blue, Black, or another? The power of your lineage awaits your answer. Available anceestries: ')
+    for ancestry in ancestries:
+        print(f"- {ancestry}")
+        
+
+    while (player.race.ancestry == 'none'):
+        chosen_ancestry = input('Enter your choice: ').lower()
+
+        if chosen_ancestry in ancestries.keys():
+            chosen_ancestry = ancestries[chosen_ancestry]
+            player.race.ancestry = chosen_ancestry
+            print(f"A fine choice indeed. Your {chosen_ancestry.dragon_type.capitalize()} dragon lineage gives you resistance to {chosen_ancestry.damage_type} damage and a {chosen_ancestry.breath_weapon} {chosen_ancestry.damage_type} breath weapon.")
+        else:
+            print('You must select an ancestry from the list above')
 
 ## ABILITY SCORES ## 
-decision = input('Do you want to manual enter your stats or have them rolled randomly? \n\nChoose: manual or random \n\n')
+# decision = input('Do you want to manual enter your stats or have them rolled randomly? \n\nChoose: manual or random \n\n')
+decision = 'random'
 
 if decision == 'random':
 
-    print('\nBehold, as the ethereal dice dance upon the digital plane, conjuring a tapestry of randomly generated scores that shall define your character\'s inherent abilities.')
+    #print('\nBehold, as the ethereal dice dance upon the digital plane, conjuring a tapestry of randomly generated scores that shall define your character\'s inherent abilities.')
 
-    randomScores = roll_random_ability_scores()
+    random_scores = roll_random_ability_scores()
     
-    for ability in randomScores:
-        print(f'{ability}: {randomScores[ability]}')
+    for ability in random_scores:
+        print(f'{ability}: {random_scores[ability]}')
+    
+    player.update_ability_scores(random_scores)
 
 elif decision == 'manual':
 
@@ -79,4 +155,4 @@ elif decision == 'manual':
                 print('\nYour score must be between 3 and 18\n')
 
 
-    print(manual_scores)
+    #print(manual_scores)
