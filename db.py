af61dd07ba4206fe3dc79906feae4e46cfb21e4c
Liam Bowie <liambowie16@gmail.com>
2023-07-20 15:58:26 +0100

    Add lifespan as an attribute of the race class to hold the descriptive text that explains a races typical lifespan. This will be shown to the user when they are asked to enter the age of their adventurer.

--- db.py
+++ db.py
@@ -6,18 +6,19 @@ available_races = {
         ability_score_increase={'str': 2, 'cha': 1}, 
         size='medium', 
         speed=30, 
+        lifespan='Young dragonborn grow quickly. They walk hours after hatching, attain the size and development of a 10-year-old human child by the age of 3, and reach adulthood by 15. They live to be around 80.',
         languages=['common', 'draconic'],  
         darkvision=False, 
         additional_traits={
             'breath weapon': 'You can use your action to exhale destructive energy. Your draconic ancestry determines the size, shape, and damage type of the exhalation. When you use your breath weapon, each creature in the area of the exhalation must make a saving throw, the type of which is determined by your draconic ancestry. The DC for this saving throw equals 8 + your Constitution modifier + your proficiency bonus. A creature takes 2d6 damage on a failed save, and half as much damage on a successful one. The damage increases to 3d6 at 6th level, 4d6 at 11th level, and 5d6 at 16th level. After you use your breath weapon, you can\'t use it again until you complete a short or long rest.'
             }
         ),
-
     'mountain dwarf' : Race(
         race_name='Mountain Dwarf', 
         ability_score_increase={'str': 2, 'con': 2}, 
         size='medium', 
         speed=25, 
+        lifespan='Dwarves mature at the same rate as humans, but they\'re considered young until they reach the age of 50. On average, they live about 350 years',
         languages=['common', 'dwarvish'], 
         resistances=['poison'], 
         weapon_proficiency=['battleaxe', 'handaxe', 'lighthammer', 'warhammer'], 
@@ -33,6 +34,7 @@ available_races = {
         ability_score_increase={'con': 2, 'wis': 1}, 
         size='medium', 
         speed=25, 
+        lifespan='Dwarves mature at the same rate as humans, but they\'re considered young until they reach the age of 50. On average, they live about 350 years',
         languages=['common, dwarvish'], 
         resistances=['poison'], 
         darkvision=True, 
