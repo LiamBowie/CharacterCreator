6b2af6d12302b0ac2073755596201764a86e7ed3
Liam Bowie <liambowie16@gmail.com>
2023-07-20 17:19:54 +0100

    Update add_race method and character constructor to include all attributes

--- character.py
+++ character.py
@@ -1,17 +1,51 @@
 import math
 
+starting_scores = {'str': 0, 'dex': 0, 'con': 0, 'wis': 0, 'int': 0, 'cha': 0}
+
 class Character:
     level = 1
     proficiency_bonus = 2
     armour_class = 10
     hit_points = 0
-    ability_scores = {'str': 0, 'dex': 0, 'con': 0, 'wis': 0, 'int': 0, 'cha': 0}
+
+    def __init__(self, name:str = '', level:int = 1, proficiency_bonus:int = 2, armour_class:int = 10, 
+                 hit_points:int = 0, ability_scores:dict = starting_scores, race:str = '', size:str = '',
+                 speed:int = 0, languages:list = [],  darkvision:bool = False, resistances:list = [],
+                 weapon_proficiency:list = [], armour_training:list = [], tool_proficiency:list = [],
+                 spells:list = [], traits:dict = {}):
+        self.name = name
+        self.level = level
+        self.proficiency_bonus = proficiency_bonus
+        self.armour_class = armour_class
+        self.hit_points = hit_points
+        self.ability_scores = ability_scores
+        self.race = race
+        self.size = size
+        self.speed = speed
+        self.languages = languages
+        self.darkvision = darkvision
+        self.resistances = resistances
+        self.weapon_proficiency = weapon_proficiency
+        self.armour_training = armour_training
+        self.tool_proficiency = tool_proficiency
+        self.spells = spells
+        self.traits = traits
 
     def add_race(self, race):
         self.race = race
-
         for ability in race.ability_score_increase:
             self.ability_scores[ability] += race.ability_score_increase[ability]
+        self.size = race.size
+        self.speed = race.speed
+        self.languages.extend(race.languages)
+        self.resistances.extend(race.resistances)
+        self.darkvision = race.darkvision
+        self.weapon_proficiency.extend(race.weapon_proficiency)
+        self.armour_training.extend(race.armour_training)
+        self.tool_proficiency.extend(race.tool_proficiency)
+        self.spells.extend(race.spells)
+        self.traits.update(race.additional_traits)
+
 
     def update_ability_scores(self, new_scores):
         for ability in new_scores:
