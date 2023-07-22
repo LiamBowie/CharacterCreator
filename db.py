f4c7174c68e38a9af5e08b90f9d13a71b65d3ede
Liam Bowie <liambowie16@gmail.com>
2023-07-22 16:39:07 +0100

    Add bard class to db.py

--- db.py
+++ db.py
@@ -482,4 +482,31 @@ classes = {
             'unarmoured defence': 'While you are not wearing any armor, your Armor Class equals 10 + your Dexterity modifier + your Constitution modifier. You can use a shield and still gain this benefit.'
         }
     ),
+    'bard': CharClass(
+        name='Bard',
+        hit_dice=8,
+        saving_throws=['dex', 'cha'],
+        available_skills=["acrobatics", "animal Handling", "arcana", "athletics", "deception", "history", "insight", "intimidation", "investigation", "medicine", "nature", "perception", "performance", "persuasion", "religion", "sleight of Hand", "stealth", "survival"],
+        no_of_skills=3,
+        starting_equipment_choices=[
+            ['rapier', 'longsword'] + get_weapon_keys_by_category('simple melee') + get_weapon_keys_by_category('simple ranged'),
+            ['diplomat\'s pack', 'entertainer\'s pack'],
+            ['lute', 'bagpipes', 'drum', 'dulcimer', 'flute', 'horn', 'lyre', 'pan flute', 'shawm', 'viol']
+        ],
+        starting_equipment_given=['leather armour', 'dagger'],
+        gold_dice='5d4',
+        armour_training=['light'],
+        weapon_prof=['simple', 'hand crossbow', 'longsword', 'rapier', 'shortsword'],
+        tool_prof=['Three musical instruments of your choice'],
+        features={
+            'bardic inspiration': '''You can inspire others through stirring words or music. To do so, you use a bonus action on your turn to choose one creature other than yourself within 60 feet of you who can hear you. That creature gains one Bardic Inspiration die, a d6.
+
+Once within the next 10 minutes, the creature can roll the die and add the number rolled to one ability check, attack roll, or saving throw it makes. The creature can wait until after it rolls the d20 before deciding to use the Bardic Inspiration die, but must decide before the DM says whether the roll succeeds or fails. Once the Bardic Inspiration die is rolled, it is lost. A creature can have only one Bardic Inspiration die at a time.
+
+You can use this feature a number of times equal to your Charisma modifier (a minimum of once). You regain any expended uses when you finish a long rest.
+
+Your Bardic Inspiration die changes when you reach certain levels in this class. The die becomes a d8 at 5th level, a d10 at 10th level, and a d12 at 15th level.
+'''
+        }
+    )
 }
