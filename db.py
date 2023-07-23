2a934eefc9b632edc1e2dc3d78239c86d3414f25
Liam Bowie <liambowie16@gmail.com>
2023-07-23 21:22:15 +0100

    Add all races from the Players Handbook

--- db.py
+++ db.py
@@ -50,42 +50,185 @@ races = {
             'dwarven toughness': 'Your hit point maximum increases by 1, and it increases by 1 every time you gain a level'
             }
         ),
-        'drow elf': Race(
-            race_name='Drow Elf',
-            ability_score_increase={'dex': 2, 'cha': 1},
-            size='medium',
-            speed=30,
-            lifespan='Although elves reach physical maturity at about the same age as humans, the elven understanding of adulthood goes beyond physical growth to encompass worldly experience. An elf typically claims adulthood and an adult name around the age of 100 and can live to be 750 years old.',
-            languages=['common', 'elvish'],
-            weapon_proficiency=['rapiers', 'shortswords', 'hand crossbows'],
-            spells=['dancing lights'],
-            additional_traits={
-                'superior darkvision': 'Accustomed to the depths of the Underdark, you have superior vision in dark and dim conditions. You can see in dim light within 120 feet of you as if it were bright light, and in darkness as if it were dim light. You can\'t discern color in darkness, only shades of gray.',
-                'keen senses': 'You have proficiency in the Perception skill.',
-                'fey ancestry': 'You have advantage on saving throws against being charmed, and magic can\'t put you to sleep.',
-                'trance': 'Elves don\'t need to sleep. Instead, they meditate deeply, remaining semiconscious, for 4 hours a day. (The Common word for such meditation is "trance.") While meditating, you can dream after a fashion; such dreams are actually mental exercises that have become reflexive through years of practice. After resting in this way, you gain the same benefit that a human does from 8 hours of sleep. \nIf you meditate during a long rest, you finish the rest after only 4 hours. You otherwise obey all the rules for a long rest; only the duration is changed.',
-                'sunlight sensitivity': 'You have disadvantage on attack rolls and on Wisdom (Perception) checks that rely on sight when you, the target of your attack, or whatever you are trying to perceive is in direct sunlight.'
-            }
-        ),
-        # TODO: Choose a cantrip and an additional language
-        'high elf': Race(
-            race_name='High Elf', 
-            ability_score_increase={'dex': 2, 'int': 1},
-            size='medium', 
-            speed=30,
-            lifespan='Although elves reach physical maturity at about the same age as humans, the elven understanding of adulthood goes beyond physical growth to encompass worldly experience. An elf typically claims adulthood and an adult name around the age of 100 and can live to be 750 years old.',
-            languages=['common', 'elvish'],
-            weapon_proficiency=['longsword', 'shortsword', 'shortbow', 'longbow'],
-            additional_traits={
-                'darkvision': 'Accustomed to twilit forests and the night sky, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can\'t discern color in darkness, only shades of gray.',
-                'keen senses': 'You have proficiency in the Perception skill',
-                'fey ancestry': 'You have advantage on saving throws against being charmed, and magic can\'t put you to sleep.',
-                'trance': 
-        '''Elves don't need to sleep. Instead, they meditate deeply, remaining semiconscious, for 4 hours a day. (The Common word for such meditation is "trance.") While meditating, you can dream after a fashion; such dreams are actually mental exercises that have become reflexive through years of practice. After resting in this way, you gain the same benefit that a human does from 8 hours of sleep.
-        If you meditate during a long rest, you finish the rest after only 4 hours. You otherwise obey all the rules for a long rest; only the duration is changed.'''
-            }
+    'drow elf': Race(
+        race_name='Drow Elf',
+        ability_score_increase={'dex': 2, 'cha': 1},
+        size='medium',
+        speed=30,
+        lifespan='Although elves reach physical maturity at about the same age as humans, the elven understanding of adulthood goes beyond physical growth to encompass worldly experience. An elf typically claims adulthood and an adult name around the age of 100 and can live to be 750 years old.',
+        languages=['common', 'elvish'],
+        weapon_proficiency=['rapiers', 'shortswords', 'hand crossbows'],
+        spells=['dancing lights'],
+        additional_traits={
+            'superior darkvision': 'Accustomed to the depths of the Underdark, you have superior vision in dark and dim conditions. You can see in dim light within 120 feet of you as if it were bright light, and in darkness as if it were dim light. You can\'t discern color in darkness, only shades of gray.',
+            'keen senses': 'You have proficiency in the Perception skill.',
+            'fey ancestry': 'You have advantage on saving throws against being charmed, and magic can\'t put you to sleep.',
+            'trance': 'Elves don\'t need to sleep. Instead, they meditate deeply, remaining semiconscious, for 4 hours a day. (The Common word for such meditation is "trance.") While meditating, you can dream after a fashion; such dreams are actually mental exercises that have become reflexive through years of practice. After resting in this way, you gain the same benefit that a human does from 8 hours of sleep. \nIf you meditate during a long rest, you finish the rest after only 4 hours. You otherwise obey all the rules for a long rest; only the duration is changed.',
+            'sunlight sensitivity': 'You have disadvantage on attack rolls and on Wisdom (Perception) checks that rely on sight when you, the target of your attack, or whatever you are trying to perceive is in direct sunlight.'
+        }
+    ),
+    # TODO: Choose a cantrip and an additional language
+    'high elf': Race(
+        race_name='High Elf', 
+        ability_score_increase={'dex': 2, 'int': 1},
+        size='medium', 
+        speed=30,
+        lifespan='Although elves reach physical maturity at about the same age as humans, the elven understanding of adulthood goes beyond physical growth to encompass worldly experience. An elf typically claims adulthood and an adult name around the age of 100 and can live to be 750 years old.',
+        languages=['common', 'elvish'],
+        weapon_proficiency=['longsword', 'shortsword', 'shortbow', 'longbow'],
+        additional_traits={
+            'darkvision': 'Accustomed to twilit forests and the night sky, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can\'t discern color in darkness, only shades of gray.',
+            'keen senses': 'You have proficiency in the Perception skill',
+            'fey ancestry': 'You have advantage on saving throws against being charmed, and magic can\'t put you to sleep.',
+            'trance': 
+    '''Elves don't need to sleep. Instead, they meditate deeply, remaining semiconscious, for 4 hours a day. (The Common word for such meditation is "trance.") While meditating, you can dream after a fashion; such dreams are actually mental exercises that have become reflexive through years of practice. After resting in this way, you gain the same benefit that a human does from 8 hours of sleep.
+    If you meditate during a long rest, you finish the rest after only 4 hours. You otherwise obey all the rules for a long rest; only the duration is changed.'''
+        }
+    ),
+    'wood elf': Race(
+        race_name='Wood Name', 
+        ability_score_increase={'dex': 2, 'wis': 1},
+        size='medium',
+        speed=35,
+        lifespan='Although elves reach physical maturity at about the same age as humans, the elven understanding of adulthood goes beyond physical growth to encompass worldly experience. An elf typically claims adulthood and an adult name around the age of 100 and can live to be 750 years old.',
+        languages=['common', 'elvish'],
+        weapon_proficiency=['longsword', 'shortsword', 'shortbow', 'longbow'],
+        additional_traits={
+            'darkvision': 'Accustomed to twilit forests and the night sky, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can\'t discern color in darkness, only shades of gray.',
+            'keen senses': 'You have proficiency in the Perception skill',
+            'fey ancestry': 'You have advantage on saving throws against being charmed, and magic can\'t put you to sleep.',
+            'trance': 
+    '''Elves don't need to sleep. Instead, they meditate deeply, remaining semiconscious, for 4 hours a day. (The Common word for such meditation is "trance.") While meditating, you can dream after a fashion; such dreams are actually mental exercises that have become reflexive through years of practice. After resting in this way, you gain the same benefit that a human does from 8 hours of sleep.
+    If you meditate during a long rest, you finish the rest after only 4 hours. You otherwise obey all the rules for a long rest; only the duration is changed.''',
+            'mask of the wild': 'You can attempt to hide even when you are only lightly obscured by foliage, heavy rain, falling snow, mist, and other natural phenomena.'
+        }
+    ),
+    # TODO: spellcasting
+    'forest gnome': Race(
+        race_name='Forest Gnome',
+        ability_score_increase={'int': 2, 'dex':1},
+        size='small',
+        speed=25,
+        lifespan='Gnomes mature at the same rate humans do, and most are expected to settle down into an adult life by around age 40. They can live 350 to almost 500 years.',
+        languages=['common', 'gnomish'],
+        additional_traits={
+            'darkvision': 'Accustomed to life underground, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can\'t discern color in darkness, only shades of gray.',
+            'gnome cunning': 'You have advantage on all Intelligence, Wisdom, and Charisma saving throws against magic.',
+            'natural illusionist': 'You know the minor illusion cantrip. Intelligence is your spellcasting ability for it.',
+            'speak with small beasts': 'Through sounds and gestures, you can communicate simple ideas with Small or smaller beasts. Forest gnomes love animals and often keep squirrels, badgers, rabbits, moles, woodpeckers, and other creatures as beloved pets.'
+        }
+    ),
+    'rock gnome': Race(
+        race_name='Forest Gnome',
+        ability_score_increase={'int': 2, 'con':1},
+        size='small',
+        speed=25,
+        lifespan='Gnomes mature at the same rate humans do, and most are expected to settle down into an adult life by around age 40. They can live 350 to almost 500 years.',
+        languages=['common', 'gnomish'],
+        additional_traits={
+            'darkvision': 'Accustomed to life underground, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can\'t discern color in darkness, only shades of gray.',
+            'gnome cunning': 'You have advantage on all Intelligence, Wisdom, and Charisma saving throws against magic.',
+            'artificer\'s lore': 'Whenever you make an Intelligence (History) check related to magic items, alchemical objects, or technological devices, you can add twice your proficiency bonus, instead of any proficiency bonus you normally apply.',
+            'tinker': 
+    '''You have proficiency with artisan's tools (tinker's tools). Using those tools, you can spend 1 hour and 10 gp worth of materials to construct a Tiny clockwork device (AC 5, 1 hp). The device ceases to function after 24 hours (unless you spend 1 hour repairing it to keep the device functioning), or when you use your action to dismantle it; at that time, you can reclaim the materials used to create it. You can have up to three such devices active at a time.
+    When you create a device, choose one of the following options:
 
-        )
+    Clockwork Toy. This toy is a clockwork animal, monster, or person, such as a frog, mouse, bird, dragon, or soldier. When placed on the ground, the toy moves 5 feet across the ground on each of your turns in a random direction. It makes noises as appropriate to the creature it represents.
+    Fire Starter. The device produces a miniature flame, which you can use to light a candle, torch, or campfire. Using the device requires your action.
+    Music Box. When opened, this music box plays a single song at a moderate volume. The box stops playing when it reaches the song's end or when it is closed.'''
+        }
+    ),
+    # TODO: choose an ability score
+    'half-elf': Race(
+        race_name='Half-Elf',
+        ability_score_increase={'cha': 2},
+        size='medium',
+        speed=30,
+        lifespan='Half-elves mature at the same rate humans do and reach adulthood around the age of 20. They live much longer than humans, however, often exceeding 180 years.',
+        languages=['common', 'elvish', 'one extra language of your choice'],
+        additional_traits={
+            'darkvision': 'Thanks to your elf blood, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can\'t discern color in darkness, only shades of gray.',
+            'fey ancestry': 'You have advantage on saving throws against being charmed, and magic can\'t put you to sleep.',
+            'skill versatility': 'You gain proficiency in two skills of your choice.'
+        }
+    ),
+    # add skill 
+    'half-orc': Race(
+        race_name='Half-Orc',
+        ability_score_increase={'str': 2, 'con': 1},
+        size='medium',
+        speed=30,
+        lifespan='Half-orcs mature a little faster than humans, reaching adulthood around age 14. They age noticeably faster and rarely live longer than 75 years.',
+        languages=['common', 'orc'],
+        additional_traits={
+            'darkvision': 'Thanks to your orc blood, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can\'t discern color in darkness, only shades of gray.',
+            'menacing': 'You gain proficiency in the Intimidation skill.',
+            'relentless endurance': 'When you are reduced to 0 hit points but not killed outright, you can drop to 1 hit point instead. You can\'t use this feature again until you finish a long rest.',
+            'savage attacks': 'When you score a critical hit with a melee weapon attack, you can roll one of the weapon\'s damage dice one additional time and add it to the extra damage of the critical hit.'
+        }
+    ),
+    'lightfoot halfling': Race(
+        race_name='Lightfoot Halfling',
+        ability_score_increase={'dex': 2, 'cha': 1},
+        size='small',
+        speed=25,
+        lifespan='A halfling reaches adulthood at the age of 20 and generally lives into the middle of his or her second century.',
+        languages=['common', 'halfling'],
+        additional_traits={
+            'lucky': 'When you roll a 1 on an attack roll, ability check, or saving throw, you can reroll the die and must use the new roll.',
+            'brave': 'You have advantage on saving throws against being frightened.',
+            'halfling nimbleness': 'You can move through the space of any creature that is of a size larger than yours.',
+            'naturally stealthy': 'You can attempt to hide even when you are obscured only by a creature that is at least one size larger than you.'
+        }
+    ),
+    'stout halfling': Race(
+        race_name='Stout Halfling',
+        ability_score_increase={'dex': 2, 'con': 1},
+        size='small',
+        speed=25,
+        lifespan='A halfling reaches adulthood at the age of 20 and generally lives into the middle of his or her second century.',
+        languages=['common', 'halfling'],
+        additional_traits={
+            'lucky': 'When you roll a 1 on an attack roll, ability check, or saving throw, you can reroll the die and must use the new roll.',
+            'brave': 'You have advantage on saving throws against being frightened.',
+            'halfling nimbleness': 'You can move through the space of any creature that is of a size larger than yours.',
+            'stout resilience': 'You have advantage on saving throws against poison, and you have resistance against poison damage.'
+        }
+    ),
+    'human': Race(
+        race_name='Human',
+        ability_score_increase={'str': 1, 'dex': 1, 'con': 1, 'wis': 1, 'int': 1, 'cha': 1},
+        size='medium',
+        speed=30,
+        lifespan='Humans reach adulthood in their late teens and live less than a century.',
+        languages=['common', 'one extra language of your choice']
+    ),
+    # TODO: Choose two ability scores
+    'human variant': Race(
+        race_name='Human Variant',
+        ability_score_increase={},
+        size='medium',
+        speed=30,
+        lifespan='Humans reach adulthood in their late teens and live less than a century.',
+        languages=['common', 'one extra language of your choice'],
+        additional_traits={
+            'skills': 'You gain proficiency in one skill of your choice.',
+            'Feat': 'You gain one feat of your choice.'
+        }
+    ),
+    'tiefling': Race(
+        race_name='Tiefling', 
+        ability_score_increase={'cha': 2, 'int': 1},
+        size='medium',
+        speed=30,
+        lifespan='Tieflings mature at the same rate as humans but live a few years longer.',
+        languages=['common', 'infernal'],
+        additional_traits={
+            'darkvision': 'Thanks to your infernal heritage, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can\'t discern color in darkness, only shades of gray.',
+            'hellish resistance': 'You have resistance to fire damage.',
+            'infernal legacy': 'You know the thaumaturgy cantrip. Once you reach 3rd level, you can cast the hellish rebuke spell as a 2nd-level spell with this trait; you regain the ability to cast it when you finish a long rest. Once you reach 5th level, you can also cast the darkness spell once per day with this trait; you regain the ability to cast it when you finish a long rest. Charisma is your spellcasting ability for these spells.'
+        }
+    )
 }
 
 draconic_ancestries = {
